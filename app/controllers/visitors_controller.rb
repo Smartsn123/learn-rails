79a336d8c8b3f988d5b9861aeb06da9d7ea843aa
Smartsn123 <smartsn123@gmail.com>
2016-03-10 12:55:00 +0530

    added Zurb Foundation Front end Framework

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,7 +2,7 @@ class VisitorsController <ApplicationController
 
         def new
                 @owner=Owner.new
-                flash.now[:notice] = 'welcome!'
-                flash.now[:alert] = 'My birthday is soon !'
+                #flash.now[:notice] = 'welcome!'
+                #flash.now[:alert] = 'My birthday is soon !'
        end
 end
