ec33d18a5d4e4edffebe4aaaa1c58f74692a9013
Smartsn123 <smartsn123@gmail.com>
2016-03-12 13:11:34 +0530

    Contact Form

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -2,6 +2,7 @@ class VisitorsController <ApplicationController
 
         def new
                 @owner=Owner.new
+                #rendering is done automatically by the controller to page views/visitors new.html.erb
                 #flash.now[:notice] = 'welcome!'
                 #flash.now[:alert] = 'My birthday is soon !'
        end
